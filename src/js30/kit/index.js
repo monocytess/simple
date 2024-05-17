6a421c50ff6b7f01501dcb1aac8117e60a547313
Pan <huiwenpan@newbanker.cn>
2024-05-17 18:33:46 +0800

    fix: 05-17 18:33

--- src/js30/kit/index.js
+++ src/js30/kit/index.js
@@ -1,6 +1,13 @@
+function isMobileDevice() {
+  return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+}
+
 function playSound(ev) {
   const audio = document.querySelector(`audio[data-key="${ev.keyCode}"]`)
-  const key = document.querySelector(`.key[data-key="${ev.keyCode}"]`)
+  let key = document.querySelector(`.key[data-key="${ev.keyCode}"]`)
+  if(isMobileDevice()){
+    key = document.querySelector(`.key-mobile[data-key="${ev.keyCode}"]`)
+  }
 
   if (!audio) return
 
@@ -19,3 +26,31 @@ function removeTransition(ev) {
 
 const keys = document.querySelectorAll('.key')
 keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+
+if (isMobileDevice()) {
+  console.log('mobile')
+  const keys = document.querySelectorAll('.key')
+  const kbd = document.querySelectorAll('kbd')
+  keys.forEach(key => {
+    key.classList.add('key-mobile')
+    key.classList.remove('key')
+
+    key.addEventListener('click', function (ev) {
+      const kbdKey = key.dataset.key
+
+      const audio = document.querySelector(`audio[data-key="${kbdKey}"]`)
+      if (!audio) return
+      audio.currentTime = 0
+      audio.play()
+      key.classList.add('playing')
+
+      setTimeout(() => {
+        key.classList.remove('playing')
+      }, 10)
+    })
+  })
+  kbd.forEach(k => {
+    k.classList.add('kbd-mobile')
+    k.classList.remove('kbd')
+  })
+}
